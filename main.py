4bd5c41b41cb503d1e5964725a20e938725016bc
papti <paptimotej@gmail.com>
2023-05-10 18:01:35 +0200

    Upload changes

---
+++ main.py
@@ -0,0 +1,15 @@
+"""
+Entry point - Odtialto budeme spustat nas program
+"""
+
+import tools.kladivo as Kladivo  # 2. sposob
+from tools.kladivo import Kladivo  # 3. sposob
+
+# nase_nove_kladivo = tools.kladivo.Kladivo
+
+# nase_kladivo = Kladivo.Kladivo()
+
+
+
+nase_kladivo = Kladivo()
+print(nase_kladivo.vaha)
---
+++ tools/kladivo.py
@@ -0,0 +1,14 @@
+class kladivo(object):
+    def __init__(self):
+        self.typ = "buracie"
+        self.vaha = 5  # kg
+
+    def buchni_kladivom(self):
+        return "Bum"
+
+if __name__ == "__main__":
+    obj = kladivo()
+    obj.vaha = 10
+    print(obj.vaha)
+    obj.buchni_kladivom()
+    print(obj.buchni_kladivom())
